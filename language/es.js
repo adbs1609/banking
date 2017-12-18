e3a3a51b994f751b59f28b397f287b8256c87413
adbs1609 <adbs1609@gmail.com>
2017-12-18 12:32:07 -0300

    Add files via upload

---
+++ language/es.js
@@ -0,0 +1,115 @@
+var Translations = {
+    "SkipBackButtonText": "Atrás",
+    "SkipForwardButtonText": "Avanzar",
+    "PlayPauseButtonText": "Reproducir/Pausar",
+    "VolumeButtonText": "Volumen",
+    "SettingsButtonText": "Ajustes",
+    "ResumeVideoPlayback": "Reanudar la reproducción de vídeo",
+
+    "QualityMenuText": "Calidad",
+    "QualityAutoText": "Auto",
+    "LanguageMenuText": "Idioma",
+    "CCMenuText": "Subtítulos",
+    "BackFromText": "Back from",
+    "OnText": "On",
+    "OffText": "Off",
+
+    "SocialIcon-facebook": "Facebook",
+    "SocialIcon-twitter": "Twitter",
+    "SocialIcon-googleplus": "Google+",
+    "SocialIcon-email": "Email",
+    "SocialIcon-link": "Website",
+    "CollapseText": "Colapso",
+    "ExpandText": "Expandir",
+
+    "PreparedForText": "Preparado para",
+
+    "CopyrightText": "Copyright 2017 Client Name",
+    "PoweredByFooterText": "Powered by",
+    "HelpFooterText": "Ayuda",
+    "PrivacyPolicyFooterText": "Política de privacidad",
+
+    /*
+     * See en.js for details
+     */
+    "CtaButtonSettings": {
+        "button1": {
+            "card": "7b480c70",
+            "label": "Botón 1",
+            "icon": "contact"
+        },
+        "button2": {
+            "card": "7b480c70",
+            "label": "Botón 2",
+            "icon": "website"
+        },
+        "button3": {
+            "card": "7b480c70",
+            "label": "Botón 3",
+            "icon": "email"
+        },
+        "button4": {
+            "card": "7b480c70",
+            "label": "Botón 4",
+            "icon": "save"
+        },
+        "button5": {
+            "url": "http://example.com/es",
+            "label": "Botón 5",
+            "icon": "share"
+        }
+    },
+
+    /*
+     * See en.js for details
+     */
+    "PromoSettings": {
+        "promo1": {
+            "trackingName": "promo1",
+            "url": "http://example.com/es",
+            "label": "Promo (Top)",
+            "desktopImage": "./images/promo_portrait.png",
+            "mobileImage": "./images/promo_landscape.png"
+        },
+        "promo2": {
+            "trackingName": "promo2",
+            "url": "http://example.com/es",
+            "label": "Promo (Bottom)",
+            "desktopImage": "./images/promo_portrait.png",
+            "mobileImage": "./images/promo_landscape.png"
+        }
+    },
+
+    /*
+     * See en.js for details
+     */
+   "ChapterSettings": [
+        {
+            "label": "Introducción",
+            "states": [
+                {
+                    "cardId": "START",
+                    "width": 60
+                }
+            ]
+        },
+        {
+            "label": "Tu Opción",
+            "states": [
+                {
+                    "cardId": "8a53e08d",
+                    "width": 20
+                }
+            ]
+        },
+        {
+            "label": "Gracias",
+            "states": [
+                {
+                    "cardId": "fbed4eb9",
+                    "width": 20
+                }
+            ]
+        }
+    ]
+}
